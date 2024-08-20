6f8094af3efac80bc89a23c21d638e44f2787ad9
devops-anilgupta <devops.anilgupta@gmail.com>
2024-08-20 18:04:48 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "tf16112023"
+    bucket         = "tf-state-complete"
     key            = "terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
